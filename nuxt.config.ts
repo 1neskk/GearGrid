6a23bffc77bc978b10f1607072ab5bb029297f2c
1neskk <contato.nesk@outlook.com>
2024-04-04 03:02:38 -0300

    dotenv test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
   css: ["./assets/css/main.css"],
 
   runtimeConfig: {
+    public: {
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
@@ -19,6 +20,7 @@ export default defineNuxtConfig({
     FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
     FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
     FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
+    }
   },
 
   postcss: {
--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -7,13 +7,13 @@ export default defineNuxtPlugin(nuxtApp => {
     const config = useRuntimeConfig();
 
     const firebaseConfig = {
-        apiKey: config.FIREBASE_API_KEY,
-        authDomain: config.FIREBASE_AUTH_DOMAIN,
-        projectId: config.FIREBASE_PROJECT_ID,
-        storageBucket: config.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID,
-        appId: config.FIREBASE_APP_ID,
-        measurementId: config.FIREBASE_MEASUREMENT_ID
+        apiKey: config.public.FIREBASE_API_KEY,
+        authDomain: config.public.FIREBASE_AUTH_DOMAIN,
+        projectId: config.public.FIREBASE_PROJECT_ID,
+        storageBucket: config.public.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: config.public.FIREBASE_MESSAGING_SENDER_ID,
+        appId: config.public.FIREBASE_APP_ID,
+        measurementId: config.public.FIREBASE_MEASUREMENT_ID
       };
 
     const app = initializeApp(firebaseConfig);
