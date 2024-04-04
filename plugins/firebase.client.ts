1f17c28d3fe7e2d224887b5c2fa6e2eb538322e4
1neskk <contato.nesk@outlook.com>
2024-04-04 03:00:10 -0300

    Implementation test of dotenv

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -3,19 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
-require('dotenv').config();
-
 export default defineNuxtPlugin(nuxtApp => {
     const config = useRuntimeConfig();
 
     const firebaseConfig = {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID
+        apiKey: config.FIREBASE_API_KEY,
+        authDomain: config.FIREBASE_AUTH_DOMAIN,
+        projectId: config.FIREBASE_PROJECT_ID,
+        storageBucket: config.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID,
+        appId: config.FIREBASE_APP_ID,
+        measurementId: config.FIREBASE_MEASUREMENT_ID
       };
 
     const app = initializeApp(firebaseConfig);
