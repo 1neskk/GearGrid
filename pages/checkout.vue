2bc106b401010c1636bfd8f4b2bf190945b9e813
1neskk <contato.nesk@outlook.com>
2024-05-13 03:07:57 -0300

    Implemented checkout function as a composable

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -5,7 +5,7 @@
             <div class="container mx-auto px-4 py-8">
                 <h1 class="text-3xl font-bold text-white mb-4">Checkout</h1>
                 <div class="bg-white rounded-lg shadow-lg p-6">
-                    <form @submit.prevent="checkout">
+                    <form @submit.prevent="submitCheckout()">
                         <div class="mb-4">
                             <label for="card-element" class="block text-sm font-medium text-gray-700">Card Details</label>
 
@@ -37,17 +37,22 @@
 </template>
 
 <script lang="ts">
-import { onBeforeMount } from 'vue';
+import { ref, onMounted, onBeforeMount } from 'vue';
+import { useRouter } from 'vue-router';
 import { useCart } from '@/composables/useCart';
 import { onAuthStateChanged, type Auth } from 'firebase/auth';
-import { useRouter } from 'vue-router';
+import { useElements, type StripeCardElement } from '@stripe/stripe-js';
+import { checkout } from '@/composables/checkout';
 
 export default {
-    async setup() {
-        const { total, clearCart } = useCart();
+    setup() {
         const router = useRouter();
         const { $auth, $stripe } = useNuxtApp();
         const { user } = useFirebaseAuth();
+        const { total } = useCart();
+        const elements = useElements();
+        const cardElement = ref<StripeCardElement | null>(null);
+        const stripe = ref($stripe);
 
         onBeforeMount(() => {
             onAuthStateChanged($auth as Auth, (firebaseUser) => {
@@ -71,36 +76,32 @@ export default {
             keywords: 'checkout, payment, buy',
         });
 
-        const stripe = await $stripe;
-
-        async function checkout() {
-            const { id: clientSecret } = await fetch('/api/payment-intent')
-            .then((res) => res.json());
-
-            const { error } = await (stripe as any).confirmCardPayment(clientSecret, {
-                payment_method: {
-                    card: (stripe as any).elements.getElement('card'),
-                },
-            });
-
-            if (error) {
-                useSonner.error('Error!', {
-                    description: error.message,
-                    duration: 2000,
-                });
-            } else {
-                useSonner.success('Success!', {
-                    description: 'Payment successful.',
-                    duration: 2000,
-                });
-
-                clearCart();
-                router.push('/success');
+        onMounted(() => {
+            if (elements.value && !cardElement.value) {
+                cardElement.value = elements.value.create('card');
+                if (cardElement.value) {
+                    cardElement.value.mount('#card-element');
+                }
             }
+        });
+
+        const submitCheckout = async () => {
+            if (stripe.value && cardElement.value) {
+                await checkout(stripe.value);
+            }
+            else {
+                useSonner.error('Error!', {
+                    description: 'Stripe is not initialized.',
+                    duration: 2000,
+                });
+            }
+        };
+        return {
+            submitCheckout,
+            total,
         }
-        return { checkout, total };
-    }
-}
+    },
+};
 
 </script>
 
